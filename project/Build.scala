c0a258c28e5278a783bbaefbbad6a4599644e2ea
Chris Neveu <mail@chrisneveu.com>
2015-08-05 21:43:58 -0400

    Set version to 1.0.1-SNAPSHOT.

--- project/Build.scala
+++ project/Build.scala
@@ -44,7 +44,7 @@ object Build extends Build {
 
    lazy val commonSettings = Defaults.defaultSettings ++ scalariformSettings ++Seq(
       organization := "com.chrisneveu",
-      version      := "1.0.0",
+      version      := "1.0.1-SNAPSHOT",
       scalaVersion := "2.11.6",
       scalacOptions ++= Seq(
          "-unchecked",
