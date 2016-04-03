90d6a959ade58122fdac2e1d48837aaf07caaba0
Chris Neveu <mail@chrisneveu.com>
2016-04-02 21:18:43 -0400

    Fix documentation.

--- macrame/src/main/scala/macrame/enums/EnumExtensions.scala
+++ macrame/src/main/scala/macrame/enums/EnumExtensions.scala
@@ -319,6 +319,6 @@ trait OrderedModular[Enum] extends Ordered[Enum] { self : EnumApi[Enum] ⇒
  * This traits provides *all* of the auto-generated functions in `EnumApi`.
  * It is rare that one actually needs all such functions and it is recommended
  * that you use a smaller subset of these functions as provided by the other traits
- * in the `macrame.enumeration` namespace.
+ * in the `macrame.enums` namespace.
  */
 trait All[Enum] extends NumericConverters[Enum] with OrderedModular[Enum] { self : EnumApi[Enum] ⇒ }
