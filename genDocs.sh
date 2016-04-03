2c12cbeab8324e09f63f691d886460d4e8b94c3a
Chris Neveu <mail@chrisneveu.com>
2016-04-02 20:45:23 -0400

    Publish docs for macrame-play.

--- genDocs.sh
+++ genDocs.sh
@@ -1,21 +1,23 @@
 #! /usr/bin/bash
 
 MACRAME_VERSION=$1
-#MACRAME_PLAY_VERSION=$2
+MACRAME_PLAY_VERSION=$2
 
 sbt "macrame/doc"
-#sbt "macrame-play/doc"
+sbt "macrame-play/doc"
 rm -rf /tmp/macrame/$MACRAME_VERSION/
-#rm -rf /tmp/macrame-play/$MACRAME_PLAY_VERSION/
-cp -r macrame/target/scala-2.11/api /tmp/macrame/$MACRAME_VERSION/
-#cp -r macrame-play/target/scala-2.11/api /tmp/macrame-play/$MACRAME_PLAY_VERSION/
+rm -rf /tmp/macrame-play/$MACRAME_PLAY_VERSION/
+mkdir -p /tmp/macrame/$MACRAME_VERSION/
+mkdir -p /tmp/macrame-play/$MACRAME_PLAY_VERSION/
+cp -r macrame/target/scala-2.11/api/* /tmp/macrame/$MACRAME_VERSION/
+cp -r macrame-play/target/scala-2.11/api/* /tmp/macrame-play/$MACRAME_PLAY_VERSION/
 
 git checkout gh-pages
 
 mkdir -p ./doc/macrame/$MACRAME_VERSION/
 mkdir -p ./doc/macrame-play/$MACRAME_PLAY_VERSION/
 cp -r /tmp/macrame/$MACRAME_VERSION/ ./doc/macrame/
-#cp -r /tmp/macrame-play/$MACRAME_PLAY_VERSION/ ./doc/macrame-play/
+cp -r /tmp/macrame-play/$MACRAME_PLAY_VERSION/ ./doc/macrame-play/
 
 git add -A
 git commit -m 'Update Scaladoc.'
