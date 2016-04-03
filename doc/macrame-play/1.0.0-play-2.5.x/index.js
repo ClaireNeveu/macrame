c0bd94d55291bf685419eeb0179b9f22548e7311
Chris Neveu <mail@chrisneveu.com>
2016-04-02 20:52:48 -0400

    Update Scaladoc.

---
+++ doc/macrame-play/1.0.0-play-2.5.x/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"macrame" : [], "macrame.play" : [{"trait" : "macrame\/play\/AsJson.html", "name" : "macrame.play.AsJson"}, {"trait" : "macrame\/play\/FromJson.html", "name" : "macrame.play.FromJson"}, {"trait" : "macrame\/play\/JsonConverters.html", "name" : "macrame.play.JsonConverters"}]};
