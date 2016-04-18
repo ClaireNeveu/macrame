8e4e18b5289ee1ec6a7de6d88827aea9fd090fe1
Chris Neveu <mail@chrisneveu.com>
2016-04-17 20:42:06 -0400

    Set version of macrame-play to 1.1.0.

--- project/Build.scala
+++ project/Build.scala
@@ -30,7 +30,7 @@ object Build extends Build {
       "macrame-play",
       file("macrame-play"),
       settings = commonSettings ++ Seq(
-         version := "1.0.3-play-2.5.x-SNAPSHOT",
+         version := "1.1.0-play-2.5.x",
          libraryDependencies ++= Seq(
             "com.chrisneveu" %% "macrame" % "[1.0,2.0[" % Provided,
             "com.typesafe.play" %% "play" % "[2.5,2.6[" % Provided,
